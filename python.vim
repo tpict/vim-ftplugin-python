tpict/vim-ftplugin-python

commit 63dd2dd1079fe869323d3516f19ff6cae9b07cc1

Add optional 'async' before 'def' in define (#21)
Previous regex would catch line starting with `def` or `class`, but not `async def`. This patch lets an optional `async ` come before a `def`.

python.vim
@@ -14,7 +14,7 @@ set cpo&vim
 setlocal cinkeys-=0#
 setlocal indentkeys-=0#
 setlocal include=^\\s*\\(from\\\|import\\)
-setlocal define=^\\s*\\(def\\\|class\\)
+setlocal define=^\\s*\\([async ]\\?def\\\|class\\)
 
 " For imports with leading .., append / and replace additional .s with ../
 let b:grandparent_match = '^\(.\.\)\(\.*\)'

commit c56985469b890b4b0c968a32b148bd73e298d87a

Fix broken 'define' option
This fixes the broken 'define' option introduced in #21.
Test this by typing Ctrl-X Ctrl-D in the following file:

    async def Foo():
        pass

python.vim
@@ -3,7 +3,7 @@
 " Maintainer:	Tom Picton <tom@tompicton.com>
 " Previous Maintainer: James Sully <sullyj3@gmail.com>
 " Previous Maintainer: Johannes Zellner <johannes@zellner.org>
-" Last Change:	2024/05/11
+" Last Change:	2024/05/13
 " https://github.com/tpict/vim-ftplugin-python
 
 if exists("b:did_ftplugin") | finish | endif
@@ -14,7 +14,7 @@ set cpo&vim
 setlocal cinkeys-=0#
 setlocal indentkeys-=0#
 setlocal include=^\\s*\\(from\\\|import\\)
-setlocal define=^\\s*\\([async ]\\?def\\\|class\\)
+setlocal define=^\\s*\\(\\(async\\s\\+\\)\\?def\\\|class\\)
 
 " For imports with leading .., append / and replace additional .s with ../
 let b:grandparent_match = '^\(.\.\)\(\.*\)'